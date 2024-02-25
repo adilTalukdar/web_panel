2bc6fe804150ddd2829ba61ea99869ef79540ec2
LAPTOP-V5IC4G6U\Adil Talukdar <adil.talukdar12j@gmail.com>
2024-02-25 15:14:47 +0530

    connect flutter web app with firebase 42

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'dashboard/side_navigation_drawer.dart';
 
-void main()
+void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCiiD4HK7QGTEDRnmIQMY5GhyQ-JLytawU",
+        authDomain: "rental-ambulance-mobile-60158.firebaseapp.com",
+        databaseURL: "https://rental-ambulance-mobile-60158-default-rtdb.firebaseio.com",
+        projectId: "rental-ambulance-mobile-60158",
+        storageBucket: "rental-ambulance-mobile-60158.appspot.com",
+        messagingSenderId: "817801668774",
+        appId: "1:817801668774:web:a24c5fca19e163f450ec67",
+        measurementId: "G-ZL2MVCVDYZ"
+    )
+  );
+  
   runApp(const MyApp());
 }
 
