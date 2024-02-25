35037ae19d0722b069fcd3282d4ad2ca299e5f87
LAPTOP-V5IC4G6U\Adil Talukdar <adil.talukdar12j@gmail.com>
2024-02-25 14:05:53 +0530

    implement headers for driver and users and trips web page part 38

--- lib/pages/drivers_pages.dart
+++ lib/pages/drivers_pages.dart
@@ -1,3 +1,4 @@
+import 'package:admin_uber_web_panel/methods/common_methods.dart';
 import 'package:flutter/material.dart';
 
 class DriversPage extends StatefulWidget
@@ -10,17 +11,51 @@ class DriversPage extends StatefulWidget
   State<DriversPage> createState() => _DriversPageState();
 }
 
-class _DriversPageState extends State<DriversPage> {
+class _DriversPageState extends State<DriversPage>
+{
+  CommonMethods cMethods = CommonMethods();
+
   @override
   Widget build(BuildContext context)
   {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "DriversPage",
-          style: TextStyle(
-              color: Colors.pinkAccent,
-              fontSize: 24
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  "Manage Drivers",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 18,
+              ),
+
+              Row(
+                children: [
+                  cMethods.header(2, "DRIVER ID"),
+                  cMethods.header(1, "PICTURE"),
+                  cMethods.header(1, "NAME"),
+                  cMethods.header(1, "CAR DETAILS"),
+                  cMethods.header(1, "PHONE"),
+                  cMethods.header(1, "TOTAL EARNINGS"),
+                  cMethods.header(1, "ACTION"),
+                ],
+              ),
+
+              //display data
+
+            ],
           ),
         ),
       ),
